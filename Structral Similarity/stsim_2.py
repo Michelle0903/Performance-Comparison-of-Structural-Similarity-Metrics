d73fe694f7272d9c25d351714b6f54f06c8cdb2f
Michelle0903 <46727021+Michelle0903@users.noreply.github.com>
2020-03-12 19:38:11 -0500

    Retrieve top k results of STSIM-2

--- Structral Similarity/stsim_2.py
+++ Structral Similarity/stsim_2.py
@@ -3,13 +3,12 @@ import cv2
 import os
 import glob
 import heapq
-
-data_dir = "/Users/yuxiao/Desktop/data/Corbis128BigExperiment_gray/"
-data = glob.glob(data_dir + "*.tiff")
-#-------------------------------------
 import torch
 import time
 from torch.utils.data import Dataset, DataLoader
+
+data_dir = "/Users/yuxiao/Desktop/data/Corbis128BigExperiment_gray/"
+data = glob.glob(data_dir + "*.tiff")
 
 class ImgData(Dataset):
 
@@ -45,7 +44,6 @@ res = []
 knum = 10
 
 for k in range(len(data)):
-#for k in range(1):
     tmp = []
     score = []
     img1name = del_path(data[k])
@@ -73,11 +71,11 @@ for k in range(len(data)):
     tmp.extend(score)
     res.append(tmp)
 
-    if k%10 == 0:
+    if k%256 == 0:
         print("%d images done"%(k+1))
 
 #-----------------------------------------
-outputfile = "./stsim_2_global_result.txt"
+outputfile = "./stsim_2_result.txt"
 with open(outputfile, 'a') as f:
     for i in range(len(res)):
         line = ''
@@ -86,23 +84,5 @@ with open(outputfile, 'a') as f:
         line = line[:-1] + '\n'
         f.write(line)
     f.close()
-"""m = Metric()
-res = []
-knum = 10
-#for k in range(len(data)):
-for k in range(1):
-    img1name = del_path(data[k])
-    res.append(img1name)
-    img1 = cv2.imread(data[k], cv2.IMREAD_GRAYSCALE)
-    stsim2 = []
-    for j in range(len(data)):
-        img2 = cv2.imread(data[j], cv2.IMREAD_GRAYSCALE)
-        score = m.STSIM2(img1, img2)
-        stsim2.append([j, score])
-    stsim2.sort(key=takesecond, reverse=True)
-    for i in range(knum):
-        top_name = del_path(data[stsim2[i][0]])
-        res.append(top_name)
-res = np.array(res).reshape(-1, knum+1)"""
 
 
